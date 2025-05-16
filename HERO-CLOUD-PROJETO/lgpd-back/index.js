5649090765a098fde8645f4f447eff0d468544e1
= <shoji.akiyama@hotmail.com>
2025-05-16 19:36:23 -0300

    Atualizado para rodar 2

--- HERO-CLOUD-PROJETO/lgpd-back/index.js
+++ HERO-CLOUD-PROJETO/lgpd-back/index.js
@@ -3,13 +3,14 @@ import pkg from "body-parser";
 import router from "./routes/router.js"
 import sequelize from "./utils/database.js";
 import association from "./models/Associations.js";
+import cors from "cors";
 
 const  app = express ();
 const { json, urlencoded } = pkg;
 
 app.use(json());
 app.use(urlencoded({ extended: true}));
-
+app.use(cors());
 
 (async () => {
     try {

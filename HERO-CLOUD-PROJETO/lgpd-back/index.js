daa31b26a96883d6cb2ecedd675932d67faaffed
= <shoji.akiyama@hotmail.com>
2025-05-16 19:09:42 -0300

    atualizando para rodar

--- HERO-CLOUD-PROJETO/lgpd-back/index.js
+++ HERO-CLOUD-PROJETO/lgpd-back/index.js
@@ -1,14 +1,14 @@
-import { express } from "express";
-import { pkg } from "body-parser";
-import { router } from "./routes/router.js"
-import { sequelize } from "./utils/database.js";
-import { association } from "./models/Associations.js";
+import express from "express";
+import pkg from "body-parser";
+import router from "./routes/router.js"
+import sequelize from "./utils/database.js";
+import association from "./models/Associations.js";
 
 const  app = express ();
-const { json, urlendcoded } = pkg;
+const { json, urlencoded } = pkg;
 
 app.use(json());
-app.use(urlendcoded({ extended: true}));
+app.use(urlencoded({ extended: true}));
 
 
 (async () => {
--- HERO-CLOUD-PROJETO/lgpd-back/routes/UserController.js
+++ HERO-CLOUD-PROJETO/lgpd-back/routes/UserController.js
@@ -1,14 +1,49 @@
-import { express } from "express";
+import express from "express";
+import userService from "../services/UserService.js";
+import multer from "multer";
+import process from "process";
+
 let router = express.Router();
 
-import userService from "../services/UserService.js";
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './images');
+    },
+    filename: function (req,file, callback) {
+        callback(null, req.body.first_name + "_" + req.body.last_name + "_" + Date.now() + file.originalname);
+    }
+});
+const upload = multer({ storage: storage }).single('file');
 
 router.post("/addUser", async function (req, res) {
+
+    upload(req, res, async function(err){
+        
+        const userModel = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            gender: req.body.gender,
+            profile_picture: req.file.path
+        }
+
+        if (err) {
+            return res.end("Error uploading file.");
+        }
+
+        const user = await userService.saveUser(userModel);
+        return res.status(200).json(user);
+
+    }
+
+    );
+
     const userModel = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
         gender: req.body.gender,
+        profile_picture: req.file.path
     }
 
     const user = await userService.saveUser(userModel);
@@ -31,15 +66,27 @@ router.delete("/deleteUser/:id", async function (req, res){
 });
 
 router.put("/updateUser/:id", async function (req, res) {
-    const userModel = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        gender: req.body.gender,
-    }
+    upload(req, res, async function (err) {
+        const userModel = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            gender: req.body.gender,
+            profile_picture: req.file.path
+        }
 
-    const user = await userService.updateUserById(req.params.id, userModel);
-    return res.status(200).json(user);
+        if (err) {
+            return res.end("Error uploading file.");
+        }
+
+        const user = await userService.updateUserById(req.params.id, userModel);
+        return res.status(200).json(user);
+    });
 });
-    
+   
+router.get('/userImage/:id', async function (req, res){
+    const user = await userService.getUserById(req.params.id);
+    res.sendFile(process.cwd() + "\\" + user.profile_picture);
+})
+
 export default router;
